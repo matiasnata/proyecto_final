7fed106913292e87cf5ae285dcf5895f596ca0e8
Lautaro Rodoni <lautarorodoni@gmail.com>
2026-05-18 17:13:06 -0300

    importo librerias al pagination.py y arreglo algunos errores que tenia mi codigo antes, tambien incluyo el blueprint

--- pagination.py
+++ pagination.py
@@ -1,31 +1,41 @@
-@app.route('/inicio', methods=['GET'])
-def pagina_principal():
-    return render_template('index.html')
+from flask import Blueprint, request, jsonify
+from database.db import get_connection
+from mysql.connector import Error
 
-@app.route('/inicio/reserva', methods=['GET'])
+reservas_bp = Blueprint("reservas", __name__)
+
+@reservas_bp.route('/reservas', methods=['GET'])
+
 def obtener_reservas():
     try:
         limit_arg = request.args.get ('_limit', default = '10')
-        offset_arg = request.arg.get ('_offset', default = '10') 
+        offset_arg = request.args.get ('_offset', default = '0') 
         if not (limit_arg.isdigit() and offset_arg.isdigit()):
             return jsonify({
                 'errors':[{
-                    "code": 400,
+                    'code': 400,
                     'message': 'Parametros invalidos',
                     'description': 'los parametros _limit y _offset deben ser numeros enteros.'
                 }]
             }), 400
+            
         limit = int(limit_arg)
         offset = int(offset_arg)
+        
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
+        
         cursor.execute('SELECT COUNT(*) as total FROM reservas')
         total_reservas = cursor.fetchone()['total']
-        cursor.execute(f'SELECT * FROM reservas LIMIT {limit} OFFSET {offset}')
+        
+        query = 'SELECT * FROM reservas LIMIT %s OFFSET %s'
+        cursor.execute(query, (limit, offset))
         resultado = cursor.fetchall()
+        
         if not resultado:
             conn.close()
             return'', 204
+        
         base_url = 'http://127.0.0.1:8080/reservas'
         ultimo_offset = max(0, ((total_reservas -1) //limit)* limit)
         prev_offset = 0
@@ -37,13 +47,13 @@ def obtener_reservas():
                 '_next': {'href': f'{base_url}?_limit={limit}&_offset={offset + limit}'},
                 '_last': {'href': f'{base_url}?_limit={limit}&_offset={ultimo_offset}'},
             }
-            
             cursor.close()
             conn.close()
             return jsonify({
                 'Reservas': resultado,
                 '_links' : links
-            }), 200
+            }), 200 
+
     except:
         return jsonify({
             'errors': [{
@@ -52,4 +62,3 @@ def obtener_reservas():
                 'description': 'Fallo interno del servidor'
             }]
         }), 500
-
