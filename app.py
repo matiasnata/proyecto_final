5d2989530a1dd4a05cf1557f8439fbab639d6b10
EliAntoniadis <elias200601@outlook.es>
2026-05-18 18:31:34 +0000

    registro blueprint menu en app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes.inicio import inicio_bp
+from routes.menu import menu_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(inicio_bp)
+app.register_blueprint(menu_bp)
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
