4f64ad43341f690cbfeacf8db329eb085096eac1
Lautaro Rodoni <lautarorodoni@gmail.com>
2026-06-01 20:13:36 -0300

    Añado conexion back-frotn para la parte de reservas

--- frontend/routes/reservas.py
+++ frontend/routes/reservas.py
@@ -1,3 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for
+
+return redirect(url_for('inicio.inicio'))
 
 reservas_bp = Blueprint("reservas", __name__)
