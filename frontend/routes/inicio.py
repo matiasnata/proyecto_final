c331841e5dc1cd780c36d139e6084bee0b78ce49
matias <armidamatias@gmail.com>
2026-06-08 19:25:44 +0000

    Agrego la conexion de servicios_extra en index.html

--- frontend/routes/inicio.py
+++ frontend/routes/inicio.py
@@ -7,6 +7,7 @@ inicio_bp = Blueprint("inicio", __name__)
 def inicio():
     platos = []
     reseñas = []
+    servicios_extra = []
     
     try:
         resultado_backend_platos = requests.get("http://127.0.0.1:5001/platos")
@@ -21,5 +22,13 @@ def inicio():
             reseñas = resultado_backend_reseñas.json().get("resultado", [])
     except requests.exceptions.ConnectionError:
         print("Error: No se pudo conectar con el Backend para obtener reseñas")
+        
+    try: 
+        resultado_backend_servicios_extra = requests.get("http://127.0.0.1:5001/restaurante/servicios-extra") 
+        if resultado_backend_servicios_extra.status_code == 200:
+            servicios_extra = resultado_backend_servicios_extra.json()
+            
+    except requests.exceptions.ConnectionError:
+        print("Error: No se pudo conectar con el Backend para obtener servicios_extra")       
     
-    return render_template("index.html", platos=platos, reseñas=reseñas)
+    return render_template("index.html", platos=platos, reseñas=reseñas,servicios_extra=servicios_extra)
