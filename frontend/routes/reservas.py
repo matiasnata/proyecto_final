e571d55d365bf0d6bc78420bae1a68f18e1ed40a
santiagomobiglia <smobiglia@fi.uba.ar>
2026-06-13 13:20:29 -0300

    feat(reservas.py frontend):Agregar los parametros limit y offset en admin_reservas para pasarle al backend y generar los links de la paginación.

--- frontend/routes/reservas.py
+++ frontend/routes/reservas.py
@@ -30,14 +30,44 @@ def crear_reserva():
 
 @reservas_bp.route("/admin/reservas")
 def admin_reservas():
+    limit = request.args.get('_limit', 5)   
+    offset = request.args.get('_offset', 0)
+    url_backend = "http:///127.0.0.1:5001/reservas/admin"
+    
+    parametros_para_backend = {
+        '_limit': limit,
+        '_offset': offset
+    }
+    
     total_reservas = []
+    link_prev = {}
+    link_next = {}
+    link_first = {}
+    link_last = {}
+    
     try:
-        response = requests.get("http://localhost:5001/reservas/admin")
+        response = requests.get("http://127.0.0.1:5001/reservas/admin", params=parametros_para_backend)
         if response.status_code == 200:
             total_reservas = response.json().get("data", [])
+            links = response.json().get("links", [])
+        
+        url_frontend_base = url_for('reservas.admin_reservas')
+        
+        def corregir_link(link_backend):
+            if link_backend and 'href' in link_backend:
+                # Reemplazamos el dominio del backend por el del frontend
+                link_backend['href'] = link_backend['href'].replace('http://127.0.0.1:5001/reservas/admin', url_frontend_base)
+            return link_backend
+        
+        
+        link_prev = corregir_link(links.get('prev', {}))
+        link_next = corregir_link(links.get('next', {}))
+        link_first = corregir_link(links.get('first', {}))
+        link_last = corregir_link(links.get('last', {}))
+        
     except Exception as e:
         print("Error al buscar reservas:", e)
-    return render_template("admin_reservas.html", usuario_autenticado="Admin", total_reservas=total_reservas)
+    return render_template("admin_reservas.html", usuario_autenticado="Admin", total_reservas=total_reservas, link_prev=link_prev, link_next=link_next, link_first=link_first, link_last=link_last)
 
 @reservas_bp.route("/admin/reservas/confirmar/<int:id_reserva>", methods=["POST"])
 def admin_confirmar_reserva(id_reserva):
