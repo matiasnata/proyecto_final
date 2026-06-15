7022a198cf66a9d7f1ea21edba3267a92eb78123
santiagomobiglia <smobiglia@fi.uba.ar>
2026-06-15 04:55:02 -0300

    fix(frontend): agrega la ruta incio.py corregida con la url sin hardcodear, me olvide de hacerlo en el commit anterior.

--- frontend/routes/inicio.py
+++ frontend/routes/inicio.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 import requests
+from config import API_BASE_URL
 
 inicio_bp = Blueprint("inicio", __name__)
 
@@ -10,21 +11,21 @@ def inicio():
     servicios_extra = []
     
     try:
-        resultado_backend_platos = requests.get("http://127.0.0.1:5001/platos")
+        resultado_backend_platos = requests.get(f"{API_BASE_URL}/platos")
         if resultado_backend_platos.status_code == 200:
             platos = resultado_backend_platos.json()
     except requests.exceptions.ConnectionError:
         print("Error: No se pudo conectar con el Backend para obtener platos")
 
     try:
-        resultado_backend_reseñas = requests.get("http://127.0.0.1:5001/reseñas?_limit=6")
+        resultado_backend_reseñas = requests.get(f"{API_BASE_URL}/reseñas?_limit=6")
         if resultado_backend_reseñas.status_code == 200:
             reseñas = resultado_backend_reseñas.json().get("resultado", [])
     except requests.exceptions.ConnectionError:
         print("Error: No se pudo conectar con el Backend para obtener reseñas")
         
     try: 
-        resultado_backend_servicios_extra = requests.get("http://127.0.0.1:5001/restaurante/servicios-extra") 
+        resultado_backend_servicios_extra = requests.get(f"{API_BASE_URL}/restaurante/servicios-extra") 
         if resultado_backend_servicios_extra.status_code == 200:
             servicios_extra = resultado_backend_servicios_extra.json()
             
